5599a3d27d1ab2062d6675eb97868e0df8e42dca
Bendricks40 <bendricks40@gmail.com>
2019-10-17 16:48:03 -0400

    more progress

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_bcrypt import check_password_hash
+from wtforms.fields.html5 import DateField
+
+import forms
+import models
 
 
 DEBUG = True
-PORT = 8000
+PORT = 8080
 HOST = '0.0.0.0'
 
 
 app = Flask(__name__)
 app.secret_key = 'djkc8@mndp40t*B)*#&$@dmf>#<#<?#<>s.D>Sfaezi'
+
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request"""
+    g.db = models.DATABASE
+    print('connected to database!')
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    """Close the database after each request"""
+    g.db.close()
+    print('closed database!')
+    return response
 
 
 @app.route('/')
@@ -19,10 +39,33 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/new')
-@app.route('/entries/new')
+@app.route('/new', methods=('GET', 'POST'))
 def create():
-    return render_template('new.html')
+    print("im in the create function")
+    form = forms.JournalEntry()
+    print("about to do the if statement")
+    print(form.title.data)
+    print("here are results of the validate on submit check: " + str(form.validate_on_submit()))
+    if form.is_submitted():
+        print("submitted")
+    if form.validate():
+        print("valid")
+    if form.validate_on_submit():
+        print("the form was validated successfully")
+        print("Title: " + form.title.data)
+        print(form.dateCreated.data)
+        print("Time Spend: " + form.timeSpent.data)
+        print("Learned: " + form.learned.data)
+        print("resources: " + form.resources.data)
+        models.Entries.create_entry(
+            title=form.title.data,
+            dateCreated=form.dateCreated.data,
+            timeSpent=form.timeSpent.data,
+            learned=form.learned.data,
+            resources=form.resources.data
+        )
+        return redirect(url_for('index'))
+    return render_template('new.html', form=form)
 
 
 @app.route('/entries/<id>')
@@ -39,5 +82,8 @@ def edit():
 def delete():
     return "This is the entries/<id>/delete page where you would be deleting a specific entry"
 
+
+if __name__ == '__main__':
+    models.initialize()
 
 app.run(debug=DEBUG, host=HOST, port=PORT)
---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import Form
+from wtforms import StringField, DateField,  PasswordField, TextAreaField
+from wtforms.validators import (DataRequired, Regexp, ValidationError, Email, Length, EqualTo)
+from wtforms.fields.html5 import DateField
+
+
+class JournalEntry(Form):
+    title = StringField('Title')
+    dateCreated = DateField('Date Created')
+    timeSpent = StringField('Time Spent')
+    learned = TextAreaField('What I learned')
+    resources = TextAreaField('Helpful resources')
+
+
